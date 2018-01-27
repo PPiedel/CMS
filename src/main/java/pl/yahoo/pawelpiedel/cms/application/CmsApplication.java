6949089ee531d1b5bd36f3b6241a5829a34e7cf7
PPiedel <pawelpiedel@yahoo.pl>
2018-01-27 12:47:37 +0100

    Home controller and basic spring security config implemented.

---
+++ src/main/java/pl/yahoo/pawelpiedel/cms/application/CmsApplication.java
@@ -0,0 +1,16 @@
+package pl.yahoo.pawelpiedel.cms.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = {"pl.yahoo.pawelpiedel.cms"})
+@EntityScan(basePackages = {"pl.yahoo.pawelpiedel.cms.model"})
+@EnableJpaRepositories("pl.yahoo.pawelpiedel.cms.repositories")
+public class CmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsApplication.class, args);
+	}
+}
---
+++ src/main/java/pl/yahoo/pawelpiedel/cms/config/SpringSecurityConfig.java
@@ -0,0 +1,32 @@
+package pl.yahoo.pawelpiedel.cms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/static/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .antMatchers("/console").permitAll()
+                .antMatchers("/resources/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**");
+    }
+}
