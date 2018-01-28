c746471fa85f3f77902e18d7a0c0291d5f9ae613
PPiedel <pawelpiedel@yahoo.pl>
2018-01-28 17:09:48 +0100

    Angular cli navbar (still need changes) implemented.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,29 +1,26 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import {MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonsModule, CollapseModule} from 'ngx-bootstrap';
 
 import {AppComponent} from './app.component';
 import {PostListComponent} from './post-list/post-list.component';
 
 import {PostService} from './shared/posts/post.service';
+import {NavbarComponent} from './shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatInputModule,
-    MatListModule,
-    MatToolbarModule
+    CollapseModule,
+    ButtonsModule
   ],
   providers: [PostService],
   bootstrap: [AppComponent]
