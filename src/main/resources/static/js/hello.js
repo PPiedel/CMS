9a46d9c8178cc1f7e9f0c7c01bf66ce782c210fb
PPiedel <pawelpiedel@yahoo.pl>
2018-01-28 20:54:22 +0100

    Angular AppComponent and AppModule defined with /posts downloading in the component constructor.

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,22 +1,26 @@
 var AppComponent = ng.core.Component({
     selector: 'app',
-    template: '<p>The ID is {{greeting.id}}</p><p>The content is {{greeting.content}}</p>'
+    template: '<div *ngFor="let post of posts">{{post.title}}</div>'
 }).Class({
-    constructor: function () {
-        this.greeting = {id: 'XYZ', content: 'Hello World'};
-    }
+    constructor: [ng.http.Http, function (http) {
+        var self = this;
+        self.posts = "";
+        http.get("/posts").subscribe(response = > self.posts = response.json()
+    )
+        ;
+        console.log(self.posts.toString());
+    }]
 });
 
 var AppModule = ng.core.NgModule({
-    imports: [ng.platformBrowser.BrowserModule],
+    imports: [ng.platformBrowser.BrowserModule, ng.http.HttpModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 }).Class(
     {
         constructor: function () {
         }
-    }
-)
+    });
 
 document.addEventListener('DOMContentLoaded', function () {
     ng.platformBrowserDynamic.platformBrowserDynamic().bootstrapModule(AppModule);
