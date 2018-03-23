52f6a75e8d67dfbc49ae86b1601eb1bca664832f
PPiedel <pawelpiedel@yahoo.pl>
2018-03-23 16:58:44 +0100

    Repositories changed from CrudRepository to JpaRepository.

--- src/main/java/pl/yahoo/pawelpiedel/cms/repositories/PostRepository.java
+++ src/main/java/pl/yahoo/pawelpiedel/cms/repositories/PostRepository.java
@@ -1,17 +1,14 @@
 package pl.yahoo.pawelpiedel.cms.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.yahoo.pawelpiedel.cms.model.Category;
 import pl.yahoo.pawelpiedel.cms.model.Post;
 
 import java.util.List;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAll();
 
     List<Post> findByAuthor_Email(String email);
-
-    List<Post> findByCategory(Category category);
 }
--- src/main/java/pl/yahoo/pawelpiedel/cms/repositories/UserRepository.java
+++ src/main/java/pl/yahoo/pawelpiedel/cms/repositories/UserRepository.java
@@ -1,10 +1,10 @@
 package pl.yahoo.pawelpiedel.cms.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.yahoo.pawelpiedel.cms.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
